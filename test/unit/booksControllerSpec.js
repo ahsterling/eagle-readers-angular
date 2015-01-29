b1e42746ebb80227b730e9f83f497f888338923b
Allie Sterling <sterlina89@gmail.com>
2015-01-29 11:41:17 -0800

    passes test for getting the correct book

--- test/unit/booksControllerSpec.js
+++ test/unit/booksControllerSpec.js
@@ -23,4 +23,21 @@ describe('Books Controller', function() {
 
 
  })
+
+ describe('bookController', function() {
+   var scope, ctrl;
+
+   beforeEach(module('eagleReadersApp'));
+   beforeEach(module('booksControllerModule'));
+
+   beforeEach(inject(function($controller, $stateParams) {
+     scope = {};
+     $stateParams.id = 1;
+     ctrl = $controller('bookController', {$scope: scope});
+   }));
+
+   it('should create "book" model', function() {
+     expect(scope.book.title).toBe("The Grapes of Wrath");
+   })
+ })
 });
