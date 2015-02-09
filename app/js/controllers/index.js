df181bfffca12d1fea58abfcea2f3b7349a1b004
Allie Sterling <sterlina89@gmail.com>
2015-02-09 09:07:10 -0800

    removes nav.js

---
+++ app/js/controllers/index.js
@@ -0,0 +1,31 @@
+
+var indexControllerModule = angular.module('indexControllerModule', []);
+
+indexControllerModule.controller('indexController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+  // $scope.user_id = localStorage.getItem('user_id')
+  // $scope.user_id = 1
+  // $http.get('http://localhost:3000/users/'+ $scope.user_id)
+  //   .success(function(data) {
+  //     $scope.user = data;
+  //   })
+
+  $scope.handleSignOutBtnClick = function() {
+    $auth.signOut()
+      .then(function(resp) {
+        // handle success response
+      })
+      .catch(function(resp) {
+        // handle error response
+
+    });
+
+  };
+
+  $rootScope.$on('auth:logout-success', function(ev) {
+    console.log("goodbye");
+  });
+
+  $rootScope.$on('auth:logout-error', function(ev, reason) {
+    console.log("error");
+  });
+}]);
