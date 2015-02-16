6f46b8512e02626ebc2767f99e845f64ee18524e
Allie Sterling <sterlina89@gmail.com>
2015-02-15 18:22:11 -0800

    doesn't show modal when no badge is earned

--- app/js/controllers/books.js
+++ app/js/controllers/books.js
@@ -116,18 +116,15 @@ booksControllerModule.controller('bookController', [
 
       $http.post("http://localhost:3000/users/"+$scope.user.id+"/books/new", {book_id: $scope.book.id, user_id: $scope.user.id})
         .success(function(data) {
-          console.log(data);
-          if (data.badges.count != 0) {
+          if (data.badges.length !== 0) {
             flashService.show(data.badges[0]);
             $scope.modalInstance = $modal.open({
               templateUrl: 'app/views/badges/badge_notice.html',
               controller: 'badgeModalController'
             });
-            console.log($scope.modalInstance)
             $scope.hasBook = true;
 
             $scope.modalInstance.closeModal = function() {
-              console.log('modal is closing')
               $scope.modalInstance.close();
             };
 
